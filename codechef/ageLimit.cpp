1a93e56848156230c85792b0940efbec7c1a091a
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2023-02-20 15:05:03 +0530

    Create ageLimit.cpp

---
+++ codechef/ageLimit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y,a;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y>>a;
+	    if(a>=x && a<y){
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	
+	return 0;
+}
