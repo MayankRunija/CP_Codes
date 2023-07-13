e98b7ce406f0056b7fa42659c03c2282619cb23c
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2023-07-13 16:22:00 +0530

    Create Door_mat.py

---
+++ hackerrank/python/Door_mat.py
@@ -0,0 +1,11 @@
+height,width=list(map(int,input().split()))
+j=1
+for i in range(height):
+    if i<height//2 :
+      print((".|."*j).center(width,"-"))
+      j+=2
+    elif i==height//2:
+        print("WELCOME".center(width,"-"))
+    elif i>height//2 :
+      j-=2
+      print((".|."*j).center(width,"-"))
