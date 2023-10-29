390cffd6cfc0c73e0f6d7479123c271c60f16413
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2023-10-29 16:26:11 +0530

    Create BitManipulationLonelyInteger.java

---
+++ hackerrank/java/BitManipulationLonelyInteger.java
@@ -0,0 +1,20 @@
+package com.javaaid.hackerrank.solutions.algorithms.bitmanipulation;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class BitManipulationLonelyInteger {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			res ^= sc.nextInt();
+		}
+		System.out.println(res);
+		sc.close();
+	}
+}
