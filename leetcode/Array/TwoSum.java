fd6a09674c0ebf0660b8f661e6d129e6dc2f33d0
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2023-12-03 02:24:08 +0530

    Update TwoSum.java

--- leetcode/Array/TwoSum.java
+++ leetcode/Array/TwoSum.java
@@ -1,14 +1,13 @@
-class Solution {
- public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> numToIndex;
-
-    for (int i = 0; i < nums.size(); ++i) {
-      if (numToIndex.count(target - nums[i]))
-        return {numToIndex[target - nums[i]], i};
-      numToIndex[nums[i]] = i;
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{numToIndex.get(complement), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution found");
     }
-
-    throw;
-  }
-};
+}
