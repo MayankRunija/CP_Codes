565cccf9137c2f1095b797f0cdec1e10a59c49d3
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-01-29 16:50:30 +0530

    Create WateringPlants.java

---
+++ leetcode/Array/WateringPlants.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int steps = 0, water = capacity;
+
+        for(int index = 0; index < plants.length; index++){
+            if(plants[index] > water){
+                steps += (index)*2;
+                water = capacity; // Refill water
+            }
+            water -= plants[index];
+            steps++;
+        }
+
+        return steps;
+    }
+}
