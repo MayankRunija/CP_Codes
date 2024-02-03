0eed84a60628e279c14155ed706ad47956e889a3
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-02-04 03:36:24 +0530

    Create CountElementsWithMaximumFrequency.java

---
+++ leetcode/Array/Count Elements With Maximum Frequency.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer, Integer> um = new HashMap<>();
+        for (int x : nums) {
+            um.put(x, um.getOrDefault(x, 0) + 1);
+        }
+
+        int maxi = 0;
+        for (int value : um.values()) {
+            maxi = Math.max(maxi, value);
+        }
+
+        int ans = 0;
+        for (int value : um.values()) {
+            if (value == maxi) {
+                ans += value;
+            }
+        }
+
+        return ans;
+    }
+}
