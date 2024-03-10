1713f7a47cfd512cdb47ee2e242aedc925cd8f3f
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-03-10 16:05:07 +0530

    Update MinimumAbsoluteDifference.java

--- leetcode/Array/MinimumAbsoluteDifference.java
+++ leetcode/Array/MinimumAbsoluteDifference.java
@@ -1,22 +1,19 @@
 class Solution {
-public List<List> minimumAbsDifference(int[] arr) {
-
-Arrays.sort(arr);
-List<List> l2 = new ArrayList<List>();
-int absDiff = Integer.MAX_VALUE;
-for(int i=0;i<arr.length-1;i++){
-int curr = arr[i+1] - arr[i];
-if(curr < absDiff){
-l2.clear();
-List l1 = Arrays.asList(arr[i],arr[i+1]);
-l2.add(l1);
-absDiff = curr;
-}else if( curr == absDiff){
-List l1 = Arrays.asList(arr[i],arr[i+1]);
-l2.add(l1);
-}
-
-}
-return l2;
-}
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                res.clear();
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == min) {
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return res;
+    }
 }
