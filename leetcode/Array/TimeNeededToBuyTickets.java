0ed11f8885d11f0c7a20e14d34a7b365f492a158
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-04-04 01:30:20 +0530

    Update TimeNeededToBuyTickets.java

--- leetcode/Array/TimeNeededToBuyTickets.java
+++ leetcode/Array/TimeNeededToBuyTickets.java
@@ -1,3 +1,22 @@
-public int timeRequiredToBuy(int[] t, int k) {
-    return IntStream.range(0, t.length).map(i -> Math.min(t[i], i > k ? t[k] - 1 : t[k])).sum();
+class Solution {
+    public static int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> q = new ArrayDeque<>();
+        int size = tickets.length, counter = 0;
+        boolean flag = true;
+        for(int i = 0;i < tickets.length;i++)
+            q.add(tickets[i]);
+        while(flag){
+            for(int i = 0;i < size && !q.isEmpty();i++){
+                int temp = q.remove() - 1;
+                if(i == k && temp == 0){
+                    flag = false;
+                    counter++;
+                    break;
+                }
+                q.add(temp);
+                if(temp >= 0) counter++;
+            }
+        }
+        return counter;
+    }
 }
