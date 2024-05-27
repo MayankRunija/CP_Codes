1ebc4ccf0215d71c9461a12036cafbde1a449af0
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-05-28 01:27:26 +0530

    Create KthDistinctStringInAnArray.java

---
+++ leetcode/String/KthDistinctStringInAnArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int n = arr.length;
+        int count = 0;
+
+        for(String a : arr){
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        
+        for(String a : arr){
+            if(map.get(a) == 1){
+                count++;
+            }
+            if(count == k){
+                return a;
+            }
+        }
+        return "";
+    }
+}
