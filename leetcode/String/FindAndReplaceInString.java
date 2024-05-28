667bdf6f700a33a713694512ba9136861e05e505
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-05-29 00:20:37 +0530

    Update FindAndReplaceInString.java

--- leetcode/String/FindAndReplaceInString.java
+++ leetcode/String/FindAndReplaceInString.java
@@ -1,24 +1,21 @@
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        StringBuilder sb = new StringBuilder();
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < indices.length; i++)
-            map.put(indices[i], i);
-        
-        int len = s.length();
-        for(int i = 0; i < len; i++){
-            if(map.containsKey(i)){
-                int indice = map.get(i);
-                if(s.substring(indices[indice], sources[indice].length() + indices[indice]).equals(sources[indice])){
-                    sb.append(targets[indice]);
-                    i+=sources[indice].length()-1;
-                    continue;
-                }
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i=0; i<indices.length; i++){
+            if (s.startsWith(sources[i],indices[i])){
+                map.put(indices[i],i);
             }
-            sb.append(s.charAt(i));
+        }  
+        StringBuilder sb=new StringBuilder();
+        for (int i=0; i<s.length();){
+            if (!map.containsKey(i)){
+                sb.append(s.charAt(i));
+                i++;
+            } else { //replace chars
+                sb.append(targets[map.get(i)]); 
+                i+=sources[map.get(i)].length();
+            }
         }
-        
         return sb.toString();
     }
 }
