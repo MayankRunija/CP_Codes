3073dd78555b41b35e70b82c88098f85adff893d
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-07-13 00:34:19 +0530

    Create RangeFrequencyQueries.java

---
+++ leetcode/BinarySearch/RangeFrequencyQueries.java
@@ -0,0 +1,13 @@
+class RangeFreqQuery {
+List<Integer>[] ids = new ArrayList[10001];
+Function<Integer, Integer> lower_bound = (pos) -> pos < 0 ? ~pos : pos;
+public RangeFreqQuery(int[] arr) {
+    for (int i = 0; i < ids.length; ++i)
+        ids[i] = new ArrayList<>();
+    for (int i = 0; i < arr.length; ++i)
+        ids[arr[i]].add(i);
+}
+public int query(int l, int r, int v) {
+    return lower_bound.apply(Collections.binarySearch(ids[v], r + 1)) - lower_bound.apply(Collections.binarySearch(ids[v], l));
+}
+}
