714b9b754e8e824a654fddc3d8fee18529855236
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2024-07-24 23:12:13 +0530

    Update MinimizeMaximumOfArray.java

--- leetcode/BinarySearch/MinimizeMaximumOfArray.java
+++ leetcode/BinarySearch/MinimizeMaximumOfArray.java
@@ -1,14 +1,10 @@
 class Solution {
-    func minimizeArrayValue(_ nums: [Int]) -> Int {
-        var prefixSum = nums[0] + nums[1]
-        var minimumMaxValue = (nums[1] > nums[0]) ? ((prefixSum % 2 == 0) ? prefixSum/2 : prefixSum/2 + 1) : nums[0]
-        if nums.count == 2 { return minimumMaxValue }
-        
-        for index in 2..<nums.count {
-            let curr = nums[index]
-            prefixSum += curr
-            minimumMaxValue = max(minimumMaxValue, prefixSum % (index + 1) == 0 ? prefixSum/(index + 1) : (prefixSum/(index + 1)) + 1)
+       public int minimizeArrayValue(int[] A) {
+        long sum = 0, res = 0;
+        for (int i = 0; i < A.length; ++i) {
+            sum += A[i];
+            res = Math.max(res, (sum + i) / (i + 1));
         }
-        return minimumMaxValue
+        return (int)res;
     }
 }
